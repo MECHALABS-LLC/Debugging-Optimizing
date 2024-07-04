7356858d5735bea971ee5f4261e76f8e98ac5fc1
Michael Baya <michaelbaya9@gmail.com>
2024-07-04 19:53:01 +0300

    Create OptV2.js

---
+++ OptV2.js
@@ -0,0 +1,48 @@
+/* 
+There is a function signFunc(x) that returns:
+
+1 if x is positive.
+-1 if x is negative.
+0 if x is equal to 0.
+You are given an integer array nums. Let product be the product of all values in the array nums.
+
+Return signFunc(product).
+*/
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function signFunc(x) {
+    if (x > 0) return 1;
+    if (x < 0) return -1;
+    return 0;
+}
+
+function arraySign(nums) {
+    let product = 1;
+    for (let num of nums) {
+        if (num === 0) return 0; // If any number is 0, the product is 0
+        product *= num;
+    }
+    return signFunc(product);
+}
+
+/*
+Example 1:
+
+Input: nums = [-1,-2,-3,-4,3,2,1]
+Output: 1
+Explanation: The product of all values in the array is 144, and signFunc(144) = 1
+
+Example 2:
+
+Input: nums = [1,5,0,2,-3]
+Output: 0
+Explanation: The product of all values in the array is 0, and signFunc(0) = 0
+
+Example 3:
+
+Input: nums = [-1,1,-1,1,-1]
+Output: -1
+Explanation: The product of all values in the array is -1, and signFunc(-1) = -1
+*/
